85bb1be6a7ccc671914cc952f0aa6c1ebcc981d8
lexwolf <alessandro.veltri@gmail.com>
2026-03-13 12:00:00 +0100

    Updated config files

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,20 @@
+# CMake generated Testfile for 
+# Source directory: /home/alessandro/GitHub/Academia/single_particle/extern/nano_geo_matrix/tests
+# Build directory: /home/alessandro/GitHub/Academia/single_particle/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(ngm_smoke_qs_s "/home/alessandro/GitHub/Academia/single_particle/build/tests/ngm_smoke_qs_s")
+set_tests_properties(ngm_smoke_qs_s PROPERTIES  _BACKTRACE_TRIPLES "/home/alessandro/GitHub/Academia/single_particle/extern/nano_geo_matrix/tests/CMakeLists.txt;3;add_test;/home/alessandro/GitHub/Academia/single_particle/extern/nano_geo_matrix/tests/CMakeLists.txt;0;")
+add_test(ngm_smoke_qs_ns "/home/alessandro/GitHub/Academia/single_particle/build/tests/ngm_smoke_qs_ns")
+set_tests_properties(ngm_smoke_qs_ns PROPERTIES  _BACKTRACE_TRIPLES "/home/alessandro/GitHub/Academia/single_particle/extern/nano_geo_matrix/tests/CMakeLists.txt;7;add_test;/home/alessandro/GitHub/Academia/single_particle/extern/nano_geo_matrix/tests/CMakeLists.txt;0;")
+add_test(ngm_smoke_mie_s "/home/alessandro/GitHub/Academia/single_particle/build/tests/ngm_smoke_mie_s")
+set_tests_properties(ngm_smoke_mie_s PROPERTIES  _BACKTRACE_TRIPLES "/home/alessandro/GitHub/Academia/single_particle/extern/nano_geo_matrix/tests/CMakeLists.txt;11;add_test;/home/alessandro/GitHub/Academia/single_particle/extern/nano_geo_matrix/tests/CMakeLists.txt;0;")
+add_test(ngm_cup_qs_smoke "/home/alessandro/GitHub/Academia/single_particle/build/tests/ngm_cup_qs_smoke")
+set_tests_properties(ngm_cup_qs_smoke PROPERTIES  _BACKTRACE_TRIPLES "/home/alessandro/GitHub/Academia/single_particle/extern/nano_geo_matrix/tests/CMakeLists.txt;19;add_test;/home/alessandro/GitHub/Academia/single_particle/extern/nano_geo_matrix/tests/CMakeLists.txt;0;")
+add_test(ngm_cup_mie_smoke "/home/alessandro/GitHub/Academia/single_particle/build/tests/ngm_cup_mie_smoke")
+set_tests_properties(ngm_cup_mie_smoke PROPERTIES  _BACKTRACE_TRIPLES "/home/alessandro/GitHub/Academia/single_particle/extern/nano_geo_matrix/tests/CMakeLists.txt;27;add_test;/home/alessandro/GitHub/Academia/single_particle/extern/nano_geo_matrix/tests/CMakeLists.txt;0;")
+add_test(ngm_cup_qs_module_smoke "/home/alessandro/GitHub/Academia/single_particle/build/tests/ngm_cup_qs_module_smoke")
+set_tests_properties(ngm_cup_qs_module_smoke PROPERTIES  _BACKTRACE_TRIPLES "/home/alessandro/GitHub/Academia/single_particle/extern/nano_geo_matrix/tests/CMakeLists.txt;35;add_test;/home/alessandro/GitHub/Academia/single_particle/extern/nano_geo_matrix/tests/CMakeLists.txt;0;")
+add_test(ngm_cup_mie_module_smoke "/home/alessandro/GitHub/Academia/single_particle/build/tests/ngm_cup_mie_module_smoke")
+set_tests_properties(ngm_cup_mie_module_smoke PROPERTIES  _BACKTRACE_TRIPLES "/home/alessandro/GitHub/Academia/single_particle/extern/nano_geo_matrix/tests/CMakeLists.txt;43;add_test;/home/alessandro/GitHub/Academia/single_particle/extern/nano_geo_matrix/tests/CMakeLists.txt;0;")
