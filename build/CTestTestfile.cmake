a23baf59c0a6ad2735fd27cb4037c2ac52ada1e7
Alessandro Veltri <alessandro.veltri@gmail.com>
2026-07-04 09:40:46 +0200

    Ignore local build artifacts

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alessandro/GitHub/Academia/single_particle/extern/nano_geo_matrix
-# Build directory: /home/alessandro/GitHub/Academia/single_particle/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
